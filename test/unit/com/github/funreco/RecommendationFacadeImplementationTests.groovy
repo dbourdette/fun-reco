39456fcdab8c1b51d330d253ea11255423ff10b6
thomas BRIEN <thomas.brien@centrale-marseille.fr>
2012-10-17 13:46:16 +0200

    start implementation of RecommendationFacade (ticket #17)

---
+++ test/unit/com/github/funreco/RecommendationFacadeImplementationTests.groovy
@@ -0,0 +1,33 @@
+package com.github.funreco;
+
+import org.junit.Test;
+
+class RecommendationFacadeImplementationTests {
+
+	
+	
+	@Test
+	void findProfile() {
+	}
+	
+	@Test
+	void findFriends() {
+	}
+ 
+	@Test
+	void findActions() {
+	}
+ 
+	@Test
+	void countActions() {
+	}
+ 
+	@Test
+	void findDefaultRecommendations() {
+	}
+ 
+	@Test
+	void findRecommendations() {
+	}
+	 
+}
