a9368e193801c3ec870a62a367c0c404a73aa46d
Angela Prestaux <angela.prestaux@gmail.com>
2012-10-31 20:43:05 +0100

    Test for Profile REST service (#21)

--- grails-app/controllers/com/github/dbourdette/api/ProfileRestController.groovy
+++ grails-app/controllers/com/github/dbourdette/api/ProfileRestController.groovy
@@ -1,41 +1,40 @@
 package com.github.dbourdette.api
 
 import grails.converters.JSON
-import org.codehaus.groovy.grails.web.json.*
 import org.codehaus.jackson.map.ObjectMapper
 
 class ProfileRestController {
 
 	RecommendationFacade recommendationFacade
-	
-		def show() {
-			response.status = 500
-			render ([error: 'Operation not allowed'] as JSON)
-		}
-	
-		def save() {
-			if (params.profile) {
-				ObjectMapper mapper = new ObjectMapper();
-				Profile profile = mapper.readValue(params.profile, Profile.class);
-				if (recommendationFacade.updateProfile(profile)) {
-					render profile as JSON
-				}else{
-					response.status = 500
-					render ([error: 'Parsing failed'] as JSON)
-				}
+
+	def show() {
+		response.status = 500
+		render ([error: 'Operation not allowed'] as JSON)
+	}
+
+	def save() {
+		if (params.profile) {
+			ObjectMapper mapper = new ObjectMapper();
+			Profile profile = mapper.readValue(params.profile, Profile.class);
+			if (recommendationFacade.updateProfile(profile)) {
+				render params.profile
 			}else{
 				response.status = 500
-				render ([error: 'Operation not allowed'] as JSON)
+				render ([error: 'Parsing failed'] as JSON)
 			}
-		}
-	
-		def update() {
+		}else{
 			response.status = 500
 			render ([error: 'Operation not allowed'] as JSON)
 		}
-	
-		def delete() {
-			response.status = 500
-			render ([error: 'Operation not allowed'] as JSON)
-		}
+	}
+
+	def update() {
+		response.status = 500
+		render ([error: 'Operation not allowed'] as JSON)
+	}
+
+	def delete() {
+		response.status = 500
+		render ([error: 'Operation not allowed'] as JSON)
+	}
 }
--- test/unit/com/github/dbourdette/api/ProfileRestTests.groovy
+++ test/unit/com/github/dbourdette/api/ProfileRestTests.groovy
@@ -1,6 +1,7 @@
 package com.github.dbourdette.api
 
 import grails.test.mixin.*
+import org.codehaus.jackson.map.ObjectMapper
 
 @TestFor(ProfileRestController)
 class ProfileRestTests {
@@ -8,10 +9,12 @@ class ProfileRestTests {
     void testSave() {
 		def facade = mockFor(RecommendationFacade) 
 		facade.demand.updateProfile(1..1) {Profile profile -> profile}
+		facade.createMock()
 		Profile profile = new Profile(facebookId: "testFB", email: "testEmail", name: "testName")
-		params.profile = profile
+		ObjectMapper mapper = new ObjectMapper()
+		String jsonString = mapper.writeValueAsString(profile)
+		params.profile = jsonString
 		controller.save()
-
 		facade.verify()
 	}
 
