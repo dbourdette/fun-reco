e8c318953184d84591411fd27af731e0aea45f82
Boutayna LAMRANI <boutayna.lamrani@centrale-marseille.fr>
2012-11-09 13:12:48 +0100

    functional test for profile and friend controller (fix #25)

--- test/functional/com/github/dbourdette/rest/ProfileRestControllerIntegrationTests.groovy
+++ test/functional/com/github/dbourdette/rest/ProfileRestControllerIntegrationTests.groovy
@@ -4,12 +4,33 @@ import groovyx.net.http.HTTPBuilder
 import groovyx.net.http.ContentType
 
 class ProfileRestControllerIntegrationTests extends GroovyTestCase {
-    def testGetProfile() {
-        def http = new HTTPBuilder('http://localhost:8080')
-        http.auth.basic 'admin', 'admin'
 
+	
+    def testGetProfile() {
+		
+		def http = new HTTPBuilder('http://localhost:8080')
+		http.auth.basic 'admin', 'admin'
+		
         def json = http.get(path: '/api/profile/1', contentType: ContentType.JSON)
 
         assert json.email == 'jdoo@mail.com'
+		assert json.name == 'John DOO'
+		
     }
-}
+	
+	def testGetFriendOfProfile(){
+		
+		def http = new HTTPBuilder('http://localhost:8080')
+		http.auth.basic 'admin', 'admin'
+		
+		def json = http.get(path: '/api/profile/1/friends', contentType: ContentType.JSON)
+		
+		assert json.facebookId.getAt(0)== '524368438'
+		assert json.name.getAt(0) == 'Olivier Lagache'
+		assert json.name.getAt(2) == 'Daniel Chan'
+			
+		}
+
+	}
+	
+
