294eec2d7ce65e5bafc11e76bb9eb63807273bc6
damien bourdette <dbourdette@gmail.com>
2012-12-09 15:49:37 +0100

    Meilleure synthaxe pour remplir le modèle dans IndexController

--- grails-app/controllers/com/github/dbourdette/IndexController.groovy
+++ grails-app/controllers/com/github/dbourdette/IndexController.groovy
@@ -8,10 +8,8 @@ class IndexController {
 	RecommendationFacade recommendationFacade
 
     def index() {
-		
         Profile profile = recommendationFacade.findProfile(params.email, params.facebookId);
-		
-		
+
         def model = [
                 email : params.email,
                 facebookId : params.facebookId,
@@ -19,17 +17,16 @@ class IndexController {
         ]
 
         if (profile == null) {
-            model["recommendation"] = recommendationFacade.findDefaultRecommendations();
-			println model["recommendation"]
-            model["actions"] = recommendationFacade.findActions(0, 10);
+            model.recommendation = recommendationFacade.findDefaultRecommendations();
+            model.actions = recommendationFacade.findActions(0, 10);
         } else {
-            model["recommendation"] = recommendationFacade.findRecommendations(profile.facebookId);
-            model["actions"] = recommendationFacade.findActions(profile.facebookId, 0, 10);
-            model["friends"] = recommendationFacade.findFriends(profile.facebookId);
+            model.recommendation = recommendationFacade.findRecommendations(profile.facebookId);
+            model.actions = recommendationFacade.findActions(profile.facebookId, 0, 10);
+            model.friends = recommendationFacade.findFriends(profile.facebookId);
         }
 		
 		if (params.errorMessage != null) {
-			model["errorMessage"] = params.errorMessage;
+			model.errorMessage = params.errorMessage;
 		}
 
         render(view: "/index", model : model)
